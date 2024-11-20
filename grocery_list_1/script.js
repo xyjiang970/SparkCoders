d3bdd23ff58eb66a28e9f3bbe7e19cf9daab190e
xyjiang970 <xyjiang970@gmail.com>
2024-11-20 09:45:30 -0500

    grocery list 1 in progress

---
+++ grocery_list_1/script.js
@@ -0,0 +1,16 @@
+// select the ul element. we need to append a newly created grocery list item to this ul
+const ul = document.querySelector(".grocery-list");
+
+// select the button element. we need an event listener on the button to tell when user clicks on it
+const button = 
+
+// select the input element. we need to extract what the user typed
+const input = 
+
+// add an event listener to the button
+button.addEventListener('click', () => {
+  // when clicking on the button, a new grocery list item should be created
+  // with the text that user typed into the input field
+  
+  // append the grocery list item into the unordered list
+});

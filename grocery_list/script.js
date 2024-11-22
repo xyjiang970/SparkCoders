380bab235217c5ae1e908406e2ade8ad0b02514d
xyjiang970 <xyjiang970@gmail.com>
2024-11-22 14:24:36 -0500

    updating grocery_list prj code

--- grocery_list/script.js
+++ grocery_list/script.js
@@ -1,11 +1,11 @@
 // select the ul element. we need to append a newly created grocery list item to this ul
-const ul = document.querySelector(".grocery-list");
+const ul = document.querySelector("ul");
 
 // select the button element. we need an event listener on the button to tell when user clicks on it
-const button = 
+const button = document.querySelector("button");
 
 // select the input element. we need to extract what the user typed
-const input = 
+const groceryList = [];
 
 // add an event listener to the button
 button.addEventListener('click', () => {
@@ -13,4 +13,24 @@ button.addEventListener('click', () => {
   // with the text that user typed into the input field
   
   // append the grocery list item into the unordered list
+  const input = document.querySelector("input");
+  groceryList.push(input.value);
+  render();
 });
+
+const render = () => {
+  ul.replaceChildren();
+
+  // take each item in input, and create an li fot it
+  // and append it to the ul
+  groceryList.forEach((groceryItem) => {
+    const li = document.createElement('li');
+    li.textContent = groceryItem;
+
+    ul.appendChild(li);
+  });
+};
+render();
+
+
+

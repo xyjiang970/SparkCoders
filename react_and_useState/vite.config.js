8365ad7ca2339b5cbb3011f46d05ea6bde9de125
xyjiang970 <xyjiang970@gmail.com>
2024-12-24 22:43:48 -0500

    deploy

--- react_and_useState/vite.config.js
+++ react_and_useState/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/react_and_useState/",
+  base: "/SparkCoders/",
   plugins: [react()],
 })
